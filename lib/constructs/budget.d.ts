b06b14a3e600a581ac53ef9a5b725b03c03137e0
jgopireddy <jagjeevanreddyg@gmail.com>
2022-07-09 14:28:59 -0400

    updating gitignore

--- lib/constructs/budget.d.ts
+++
@@ -1,9 +0,0 @@
-import { Construct } from 'constructs';
-interface BudgetProps {
-    budgetAmount: number;
-    emailAddress: string;
-}
-export declare class Budget extends Construct {
-    constructor(scope: Construct, id: string, props: BudgetProps);
-}
-export {};
