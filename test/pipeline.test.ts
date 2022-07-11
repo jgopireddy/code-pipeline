385f9ea4479f0cdca6957ca2f6b86fc799465790
jgopireddy <jagjeevanreddyg@gmail.com>
2022-07-11 09:29:59 -0400

    fixing the test for pipeline

--- test/pipeline.test.ts
+++ test/pipeline.test.ts
@@ -5,12 +5,14 @@ import {Template, Capture} from 'aws-cdk-lib/assertions';
 
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/pipeline-stack.ts
-test('Empty Stack Created', () => {
+test('Pipeline Stack Created', () => {
   const app = new cdk.App();
 //     // WHEN
   const stack = new Pipeline.PipelineStack(app, 'MyTestStack');
 //     // THEN
   const template = Template.fromStack(stack);
 
-   template.resourceCountIs('AWS::SQS::Queue', 0);
+  //template.resourceCountIs('AWS::SQS::Queue', 0);
+
+  expect(template).toMatchSnapshot();
 });
